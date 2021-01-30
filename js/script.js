52eb281a1895328a8e87f1da8de9351098bf9b8b
Fredy <fredy.ly.wcs@gmail.com>
2021-01-30 13:44:40 +0100

    cleaning files - commentaries, indentation

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
-// Javascript file
-
 // PAGE CONTACT
-const messageForm = "Your message has been sent. King Aeetes will give you an answer within 24h. Thank you."
-
+const messageForm = "Your message has been sent. King Aeetes will give you an answer within 24h. Thank you.";
 document
 	.getElementById("contactForm")
 	.addEventListener("submit",
@@ -12,10 +9,8 @@ document
 		}
 	);
 
-
-//Social networks pop-up
-const message_reseaux_sociaux = "Unfortunately, social networks are not operational yet. Try again in 27 or 28 centuries."
-
+// Social networks pop-up
+const message_reseaux_sociaux = "Unfortunately, social networks are not operational yet. Try again in 27 or 28 centuries.";
 document
 	.getElementById("reseaux_sociaux")
 	.addEventListener("click",
@@ -25,24 +20,19 @@ document
 		}
 	);
 
-
-//scrollUp button
+// ScrollUp button
 var mybutton = document.getElementById("scrollUp");
-
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
 	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
 		mybutton.style.display = "block";
-		}
-	else {
+	}	else {
 		mybutton.style.display = "none";
-		}
 	}
+}
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
 	document.body.scrollTop = 0;
 	document.documentElement.scrollTop = 0;
-	}
+}
